5290714a597d57fc92126954cca1cc5c82d5d24a
khenadug <77457378+khenadug@users.noreply.github.com>
2021-02-02 17:50:46 +0000

    Update settings.py

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static)
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
