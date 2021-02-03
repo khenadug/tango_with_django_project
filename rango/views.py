24009256d6a7a7104ca07de53abeaa6fe9d6e7c7
khenadug <77457378+khenadug@users.noreply.github.com>
2021-02-03 16:44:13 +0000

    Update views.py

--- rango/views.py
+++ rango/views.py
@@ -6,3 +6,7 @@ def index(request):
   context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
   return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+  
+  return render(request, 'rango/about.html', context=conetxt_dict)
