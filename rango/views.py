7cfb3c4daf596b602328beccd8d42beb7dd0c979
khenadug <77457378+khenadug@users.noreply.github.com>
2021-02-06 18:08:01 +0000

    Update views.py

--- rango/views.py
+++ rango/views.py
@@ -12,4 +12,4 @@ def index(request):
 def about(request):
    
   
-  return HttpResponse('Rango says here is the about page.')
+  return HttpResponse('Rango says here is the about page.', <a href='/rango/'>Index</a>)
