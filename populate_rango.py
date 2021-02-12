fbdeec910328c2edf8cd98bb564faf715023d69c
“Khena <2462068d@student.gla.ac.uk>
2021-02-12 19:25:45 +0000

    population script

--- populate_rango.py
+++ populate_rango.py
@@ -29,9 +29,9 @@ def populate():
         {'title':'Flask',
          'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python' : {'pages': python_pages},
-            'Django' : {'pages': django_pages},
-            'Other Frameworks' : {'pages': other_pages}}
+    cats = {'Python' : {'pages': python_pages,'views':views,'likes':likes},
+            'Django' : {'pages': django_pages,'views':views,'likes':likes},
+            'Other Frameworks' : {'pages': other_pages,'views':views,'likes':likes}}
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
@@ -52,8 +52,8 @@ def add_page(cat,title, url, views=0):
 
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name,views=0,likes=0):
+    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
     c.save()
 
     return c
